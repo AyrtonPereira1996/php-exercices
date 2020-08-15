73c7718da3f5f776a069af58dea2b5620070f91c
AyrtonPereira1996 <ayrton_pereira1996@hotmail.com>
2020-08-15 20:55:11 +0200

    add exercices 1, 2 and 3

---
+++ _exercicio2/variaveis.php
@@ -0,0 +1,13 @@
+<?php
+
+    $nome = "Programador";
+    $numeroX = 2;
+    $numeroY = 3.6;
+
+    echo "Nome: $nome;<br> Numero x: $numeroX;<br> Numero y:  $numeroY";
+
+
+
+
+
+?>
---
+++ _exercicio3/operacoesAritmetricas.php
@@ -0,0 +1,17 @@
+<?php
+    $x = 5;
+    $y = 6;
+    $soma = $x + $y;
+    $subtracao = $x - $y;
+    $multiplicacao = $x * $y;
+    $divisao = $x/$y;
+    $resto = $X%$y;
+
+    echo "Soma de x e y corresponde a $soma <br/>";
+    echo "Subtracao de x e y corresponde a $subtracao <br/>";
+    echo "Multiplicacao de x e y corresponde a $multiplicacao <br/>";
+    echo "Divisao de x e y corresponde a $divisao <br/>";
+    echo "Resto de x e y corresponde a $resto <br/>";
+
+
+?>
